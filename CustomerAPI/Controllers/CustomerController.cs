6a96d9b4b8dbebed0eb45d202dfdb5940f393adf
Mahmut <Mahmut@DESKTOP-TEJCF1U>
2024-05-22 22:39:32 +0300

    Add project files.

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> {"customer1","customer2" });
+        }
+    }
+}
