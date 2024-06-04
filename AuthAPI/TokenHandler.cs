f76df72a2c00fee220d4ddca40c8ecab4299daa2
Mahmut Türkan <136835943+Mahmuttrkn@users.noreply.github.com>
2024-06-04 09:47:54 +0300

    Update TokenHandler.cs

--- AuthAPI/TokenHandler.cs
+++ AuthAPI/TokenHandler.cs
@@ -1,41 +1,44 @@
-﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-public static class TokenHandler
+namespace AuthAPI
 {
-    public static IConfiguration _configuration;
-    public static dynamic CreateAccessToken()
+    public static class TokenHandler
     {
-        SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["JWT:Security"]));
-        TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+        public static IConfiguration _configuration;
+        public static dynamic CreateAccessToken()
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = symmetricSecurityKey,
-            ValidateIssuer = true,
-            ValidIssuer = _configuration["JWT:Issuer"],
-            ValidateAudience = true,
-            ValidAudience = _configuration["JWT:Audience"],
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            RequireExpirationTime = true
-        };
-        DateTime now = DateTime.UtcNow;
-        JwtSecurityToken jwt = new JwtSecurityToken(
-                 issuer: _configuration["JWT:Issuer"],
-                 audience: _configuration["JWT:Audience"],
-                 claims: new List<Claim> {
+            SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["JWT:Security"]));
+            TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = symmetricSecurityKey,
+                ValidateIssuer = true,
+                ValidIssuer = _configuration["JWT:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = _configuration["JWT:Audience"],
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true
+            };
+            DateTime now = DateTime.UtcNow;
+            JwtSecurityToken jwt = new JwtSecurityToken(
+                     issuer: _configuration["JWT:Issuer"],
+                     audience: _configuration["JWT:Audience"],
+                     claims: new List<Claim> {
                       new Claim(ClaimTypes.Name, "mhmt")
-                 },
-                 notBefore: now,
-                 expires: now.Add(TimeSpan.FromMinutes(2)),
-                 signingCredentials: new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256)
-             );
-        return new
-        {
-            AccessToken = new JwtSecurityTokenHandler().WriteToken(jwt),
-            Expires = TimeSpan.FromMinutes(2).TotalSeconds
-        };
+                     },
+                     notBefore: now,
+                     expires: now.Add(TimeSpan.FromMinutes(2)),
+                     signingCredentials: new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256)
+                 );
+            return new
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(jwt),
+                Expires = TimeSpan.FromMinutes(2).TotalSeconds
+            };
+        }
     }
 }
