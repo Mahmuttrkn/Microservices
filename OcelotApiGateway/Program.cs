1660cbeffd7a1ee473e6f61563d3fed317a9419e
Mahmut Türkan <136835943+Mahmuttrkn@users.noreply.github.com>
2024-06-04 09:16:31 +0300

    Update Program.cs

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Ocelot.Configuration;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.ServiceDiscovery.Providers;
+using Ocelot.Values;
+using OcelotgatewayAPI;
 using System.Text;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
 
-var signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Security"]));
+// Add services to the container.
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Configuration.AddJsonFile($"ocelot.json", false, true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var configuration = builder.Configuration; // IConfiguration nesnesini alıyoruz.
+
+SymmetricSecurityKey signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Security"]));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -25,11 +46,39 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             RequireExpirationTime = true
         };
     });
+builder.Services.AddAuthorization();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpcontext =>
+    RateLimitPartition.GetFixedWindowLimiter(
+        partitionKey: httpcontext.Request.Headers.Host.ToString(),
+        factory: partition => new FixedWindowRateLimiterOptions
+        {
+            AutoReplenishment = true,
+            PermitLimit = 3,
+            Window = TimeSpan.FromSeconds(5)
+        }
+    ));
+
+    //İsteklerin engellenmesi durumunda verilence hata kodu ve mesaj bilgisi.
+    options.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = 429;
+
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            await context.HttpContext.Response.WriteAsync(
+                $"İstek sınır sayısına ulaştınız. {retryAfter.TotalSeconds} saniye sonra tekrar deneyiniz. ", cancellationToken: token);
+        }
+        else
+        {
+            await context.HttpContext.Response.WriteAsync(
+                "İstek sınırına ulaştınız. Daha sonra tekrar deneyin. ", cancellationToken: token);
+        }
+    };
+});
+
 
 var app = builder.Build();
 
@@ -39,12 +88,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-await app.UseOcelot();
+
+
 app.MapControllers();
+app.UseRateLimiter();
+
+await app.UseOcelot();
 
 app.Run();
